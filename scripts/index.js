42647a850cf1c67543df11948578634b09f10c3a
Palak Gada <96110612+Palak0901@users.noreply.github.com>
2023-01-04 17:00:47 +0530

    Changed version

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ var exec = require('child_process').exec,
   child;
 
 child = exec(
-  `cd .. && cd .. && npm install axios react-native-share react-native-screens react-native-svg @react-native-masked-view/masked-view @react-navigation/native @react-navigation/native-stack react-native-background-timer react-native-gesture-handler react-native-responsive-dimensions react-native-safe-area-context`,
+  `cd .. && cd .. && npm install axios react-native-share react-native-screens react-native-svg @react-native-masked-view/masked-view @react-navigation/native@6.0.0 @react-navigation/native-stack@6.0.0. react-native-background-timer react-native-gesture-handler react-native-responsive-dimensions react-native-safe-area-context`,
   function (error, stdout, stderr) {
     console.log('Successfully installed' + stdout);
     console.log('Thanks For Downloading Package');
